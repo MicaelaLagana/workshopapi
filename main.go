583ecb1e7a24282f3486ca10d661d5c9610b3f8c
Micaela Romina Laganá <micaelal@lagash.com>
2019-12-16 11:18:19 -0300

    finished rest api with mux

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -32,6 +34,7 @@ func getTodo(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r) // Set a params variable and send the request, this gets the params
 
 	//Look for the todo with the id
+	//_ avoids to declare a variable
 	for _, selectedTodo := range todos {
 		if selectedTodo.ID == params["id"] {
 			json.NewEncoder(w).Encode(selectedTodo)
@@ -45,16 +48,56 @@ func getTodo(w http.ResponseWriter, r *http.Request) {
 // Post
 func createTodo(w http.ResponseWriter, r *http.Request) {
 
+	var newTodo Todo
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "The todo task has been created")
+	}
+
+	json.Unmarshal(reqBody, &newTodo)
+	todos = append(todos, newTodo)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newTodo)
+
 }
 
-// Update
+// Update, remove the item and then create it again using the same ID
 func updateTodo(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, selectedTodo := range todos {
+		if selectedTodo.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+
+			var newTodo Todo
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Fprintf(w, "The todo task has been created")
+			}
+			json.Unmarshal(reqBody, &newTodo)
+			newTodo.ID = params["id"]
+			todos = append(todos, newTodo)
+			json.NewEncoder(w).Encode(newTodo)
+		}
+	}
+
+	json.NewEncoder(w).Encode(todos)
 
 }
 
 //Delete
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, selectedTodo := range todos {
+		if selectedTodo.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(todos)
 }
 
 func main() {
@@ -72,7 +115,7 @@ func main() {
 	router.HandleFunc("/api/todo/{id}", getTodo).Methods("GET")
 	router.HandleFunc("/api/todo", createTodo).Methods("POST")
 	router.HandleFunc("/api/todo/{id}", updateTodo).Methods("PUT")
-	router.HandleFunc("/api/todo", deleteTodo).Methods("DELETE")
+	router.HandleFunc("/api/todo/{id}", deleteTodo).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 
